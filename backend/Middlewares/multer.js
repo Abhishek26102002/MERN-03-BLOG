219d945bcd13e5e357457f2f88a5858a08d85273
Abhishek Shaw <145495694+Abhishek26102002@users.noreply.github.com>
2025-01-09 16:10:06 +0530

    Update multer.js

--- backend/Middlewares/multer.js
+++ backend/Middlewares/multer.js
@@ -3,15 +3,13 @@ const path = require("path");
 
 // Configure Multer
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "images"); // Destination folder for uploaded files
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
   },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + path.extname(file.originalname)); // Unique filename
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
-
 // File filter to accept only image files
 const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image/")) {
